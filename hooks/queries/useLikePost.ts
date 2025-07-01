ac2e05de695353214d680f1cd7b0ec4c81954f9a
= <=>
2025-07-01 14:21:14 +0900

    좋아요 기능에 낙관적 업데이트 적용

--- hooks/queries/useLikePost.ts
+++ hooks/queries/useLikePost.ts
@@ -1,14 +1,54 @@
 import { likePost } from "@/api/post";
 import { queryClient } from "@/api/queryClient";
 import { queryKeys } from "@/constants";
+import { Post, Profile } from "@/types";
 import { useMutation } from "@tanstack/react-query";
 
 export default function useLikePost() {
   return useMutation({
     mutationFn: likePost,
-    onSuccess: (postId) => {
-      queryClient.invalidateQueries({
+    onMutate: async (postId) => {
+      await queryClient.cancelQueries({
         queryKey: [queryKeys.POST, queryKeys.GET_POST, postId],
+      });
+
+      const previousPost = queryClient.getQueryData<Post>([
+        queryKeys.POST,
+        queryKeys.GET_POST,
+        postId,
+      ]);
+
+      const user = queryClient.getQueryData<Profile>([
+        queryKeys.AUTH,
+        queryKeys.GET_ME,
+      ]);
+
+      const userId = Number(user?.id);
+
+      const newPost = { ...previousPost };
+      const likedIndex =
+        previousPost?.likes?.findIndex((like) => like.userId === userId) ?? -1;
+
+      likedIndex >= 0
+        ? newPost.likes?.splice(likedIndex, 1)
+        : newPost.likes?.push({ userId });
+
+      queryClient.setQueryData(
+        [queryKeys.POST, queryKeys.GET_POST, postId],
+        newPost
+      );
+
+      return { previousPost, newPost };
+    },
+    onError: (error, newPost, context) => {
+      queryClient.setQueryData(
+        [queryKeys.POST, queryKeys.GET_POST, context?.previousPost?.id],
+        context?.previousPost
+      );
+    },
+    onSettled: (data, error, variables, context) => {
+      queryClient.invalidateQueries({
+        queryKey: [queryKeys.POST, queryKeys.GET_POST, variables],
       });
       queryClient.invalidateQueries({
         queryKey: [queryKeys.POSTS, queryKeys.GET_POSTS],
